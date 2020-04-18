b3d7a2c1ff43152cab1fdd024f4379fa6624be64
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-04-18 17:10:08 +0200

    Start building main file

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ Thibault LEPEZ, Matthieu GEDEON, Daniil ROSSO
 L3 int - G2
 """
 from Graph import *
+from inputs import getFilePathFromGraphNumber
+from outputs import printMatrix
 
-graph = Graph(filename="./graphs/graph1.txt")
-print(graph)
+start = True
+
+while(start):
+    filepath = getFilePathFromGraphNumber()
+
+    print("[i] Reading graph from " + filepath)
+    graph = Graph(filename=filepath)
+
+    print(graph)
+
+    print("[i] adjustement matrix")
+    adjencyMatrix = graph.getAdjencyMatrix()
+    printMatrix(adjencyMatrix)
+
+    print("[i] values matrix")
+    valueMatrix = graph.getValueMatrix()
+    printMatrix(valueMatrix)
+
+    choice = input("Start again with another graph? [y/n]: ")
+    start = (choice == 'y' or choice == 'Y')
